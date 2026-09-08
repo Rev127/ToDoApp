fbf1cf6e6f60fbf11997df2145d557b3ae5a30fe
Taras <zanchuk20042017@gmail.com>
2026-09-08 18:36:39 +0300

    implemented-user-service

---
+++ ToDoApp.Services/Services/UserService.cs
@@ -0,0 +1,45 @@
+﻿using ToDoApp.Services.Interfaces;
+using ToDoApp.Services.Dtos.UserDtos;
+using ToDoApp.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace ToDoApp.Services.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly ToDoAppContext context;
+
+        public UserService(ToDoAppContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<GetUserDtocs> GetUserByIdAsync(string id)
+        {
+            var user = await context.Users.FindAsync(id);
+
+            return new GetUserDtocs
+            {
+                Name = user.UserName
+            };
+        }
+
+        public async Task<List<GetUserDtocs>> GetAllUsersAsync()
+        {
+            return await context.Users.Select(u => new GetUserDtocs
+            {
+                Name = u.UserName
+            }).ToListAsync();
+        }
+
+        public async Task<GetUserDtocs> GetUserByNameAsync(string name)
+        {
+            var user = context.Users.Select(u => new GetUserDtocs
+            {
+                Name = u.UserName
+            }).FirstOrDefault(u => u.Name == name);
+
+            return user;
+        }
+    }
+}
